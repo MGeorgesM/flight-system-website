2e870d98dcd2609fac9242480c9ea22ed216557c
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-03-16 20:49:12 +0200

    Add email existence check during user registration

--- backend/signup.php
+++ backend/signup.php
@@ -1,1 +1,16 @@
+<?php
+include('connection.php');
+
+
+$name = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+
+$check_email = $mysqli->prepare('select email from users where email=?');
+$check_email->bind_param('s', $email);
+$check_email->execute();
+$check_email->store_result();
+$email_exists = $check_email->num_rows();
 
