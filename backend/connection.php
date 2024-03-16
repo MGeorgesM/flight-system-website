f95f1615d72352216020e032b2c8164c5c34a006
George <g.m.6@live.com>
2024-03-16 15:44:36 +0200

    header options added to connection.php

--- backend/connection.php
+++ backend/connection.php
@@ -1,4 +1,9 @@
 <?php
+
+header('Access-Control-Allow-Origin: http://127.0.0.1:5500');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Content-Type: application/json');
+
 $host = 'localhost';
 $db_user = 'root';
 $db_pass = null;
