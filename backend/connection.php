14e1a967704d6bd958f020f4f6d26fcab0aa908d
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-03-16 20:40:44 +0200

    update connection

--- backend/connection.php
+++ backend/connection.php
@@ -8,9 +8,12 @@ $host = 'localhost';
 $db_user = 'root';
 $db_pass = null;
 $db_name = 'flightmanagementdb';
+$db_port = 3307;
 
-$mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
+$mysqli = new mysqli($host, $db_user, $db_pass, $db_name,$db_port);
 
 if ($mysqli->connect_error) {
     die('Connection Failed: ' . $mysqli->connect_error);
+}else{
+    echo "connected";
 }
