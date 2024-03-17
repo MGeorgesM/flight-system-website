53c1854f4cb7f101b1e1aac90158e063a2e3d59d
afif-ayoubi <afifayoubi22@gmail.com>
2024-03-17 13:47:59 +0200

    nothing

--- backend/connection.php
+++ backend/connection.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Content-Type: application/json');
 $host = 'localhost';
 $db_user = 'root';
 $db_pass = null;
