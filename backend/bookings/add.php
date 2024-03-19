b5ae2349729e1b9bc7bb18ad491041be94aab0cd
afif-ayoubi <afifayoubi22@gmail.com>
2024-03-19 10:33:42 +0200

    ..

--- backend/bookings/add.php
+++ backend/bookings/add.php
@@ -20,7 +20,6 @@ if($find_user->num_rows == 0) {
 }
 
 
-//Check if user has empty fields
 $find_user->bind_result($id, $username, $email, $password, $first_name, $last_name, $address, $passport_number, $coins);
 $find_user->fetch();
 
