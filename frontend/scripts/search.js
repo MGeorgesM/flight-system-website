3f1781910f20e3cd2cfdf94fd48494be8bd02133
George <g.m.6@live.com>
2024-03-20 00:30:53 +0200

    selecting flights

--- frontend/scripts/search.js
+++ frontend/scripts/search.js
@@ -6,39 +6,40 @@ let selectBtns = [];
 let departureFlights = [];
 let returnFlights = [];
 
-const resetSearchSection = () => {
-  return `<div class="select-section-title dark-text">
-            <h1>Select Departure</h1>
+const resetSearchSection = (direction) => {
+    const color = direction === 'Departure' ? 'dark-text' : 'off-white-text';
+    return `<div class="select-section-title ${color}">
+            <h1>Select ${direction}</h1>
         </div>`;
 };
 
 const populateRatingStars = (rating) => {
-  let ratingStars = '';
+    let ratingStars = '';
 
-  for (let i = 0; i < Math.round(rating); i++) {
-    ratingStars += `<i class="fa-solid fa-star"></i>`;
-  }
+    for (let i = 0; i < Math.round(rating); i++) {
+        ratingStars += `<i class="fa-solid fa-star"></i>`;
+    }
 
-  for (let i = 0; i < 5 - Math.round(rating); i++) {
-    ratingStars += `<i class="fa-regular fa-star"></i>`;
-  }
+    for (let i = 0; i < 5 - Math.round(rating); i++) {
+        ratingStars += `<i class="fa-regular fa-star"></i>`;
+    }
 
-  return ratingStars;
+    return ratingStars;
 };
 
 const populateSearchCard = async (flight) => {
-  const airlineName = await getAirlines(flight.airline_id);
-  const airlineRating = await getFlightOrAirlineAverageRating(flight.airline_id, 'airline');
-  const airlineRatingStars = populateRatingStars(airlineRating);
+    const airlineName = await getAirlines(flight.airline_id);
+    const airlineRating = await getFlightOrAirlineAverageRating(flight.airline_id, 'airline');
+    const airlineRatingStars = populateRatingStars(airlineRating);
 
-  const flightRating = await getFlightOrAirlineAverageRating(flight.id, 'flight');
-  const flightRatingStars = populateRatingStars(flightRating);
+    const flightRating = await getFlightOrAirlineAverageRating(flight.id, 'flight');
+    const flightRatingStars = populateRatingStars(flightRating);
 
-  return populateSearchCardHtml(airlineName.airline_name, airlineRatingStars, flight, flightRatingStars);
+    return populateSearchCardHtml(airlineName.airline_name, airlineRatingStars, flight, flightRatingStars);
 };
 
 const populateSearchCardHtml = (airlineName, airlineRatingStars, flight, flightRatingStars) => {
-  return `<div class="result-card flex off-white-bg box-shadow primary-text border-radius">
+    return `<div class="result-card flex off-white-bg box-shadow primary-text border-radius">
             <div class="result-card-location flex space-around">
                 <h2 class="departure-location-display">${flight.departure_location}</h2>
                 <div class="arrow-img flex center">
@@ -64,48 +65,64 @@ const populateSearchCardHtml = (airlineName, airlineRatingStars, flight, flightR
 };
 
 const adjustBtn = (btn) => {
-  console.log('adjusting');
-  btn.innerHTML === 'Selected' ? 'Select' : 'Selected';
-  btn.classlist.toggle('clicked');
+    console.log('adjusting');
+    btn.innerHTML === 'Selected' ? 'Select' : 'Selected';
+    btn.classlist.toggle('clicked');
 };
 
-const getSearchResult = () => {
-  departureFlights = JSON.parse(localStorage.getItem('departureFlights'));
-  returnFlights = JSON.parse(localStorage.getItem('returnFlights'));
+const getSearchResult = async () => {
+    searchSectionDeparture.innerHTML = resetSearchSection('Departure');
+    searchSectionReturn.innerHTML = resetSearchSection('Return');
+    departureFlights = JSON.parse(localStorage.getItem('departureFlights'));
+    returnFlights = JSON.parse(localStorage.getItem('returnFlights'));
 
-  departureFlights &&
+    departureFlights &&
+        departureFlights.forEach(async (flight) => {
+            const html = await populateSearchCard(flight);
+            searchSectionDeparture.innerHTML += html;
+            addSelectBtnEventListener(searchSectionDeparture);
+        });
+
+    returnFlights &&
+        returnFlights.forEach(async (flight) => {
+            const html = await populateSearchCard(flight);
+            searchSectionReturn.innerHTML += html;
+            addSelectBtnEventListener(searchSectionReturn);
+        });
+};
+
+const addSelectBtnEventListener = (section) => {
+    const selectBtns = section.querySelectorAll('.select-btn');
+
+    selectBtns.forEach((btn) => {
+        btn.removeEventListener('click', handleButtonClick);
+    });
+
+    selectBtns.forEach((btn) => {
+        btn.addEventListener('click', handleButtonClick);
+    });
+};
+
+const handleButtonClick = (event) => {
+    const flightId = event.target.getAttribute('flight-id');
+
     departureFlights.forEach((flight) => {
-      populateSearchCard(flight).then((html) => {
-        searchSectionDeparture.innerHTML += html;
-      });
+        if (flight.id === flightId) {
+            localStorage.setItem('selectedDepartureFlight', JSON.stringify(flight));
+        } else {
+            localStorage.setItem('selectedReturnFlight', JSON.stringify(flight));
+        }
     });
 
-  returnFlights &&
-    returnFlights.forEach((flight) => {
-      populateSearchCard(flight).then((html) => {
-        searchSectionReturn.innerHTML += html;
-      });
-    });
-    
-
-    addEventListener();  
+    localStorage.setItem('selectedFlight', flightId);
 };
 
-const populateCards = ()
-
-
-selectBtns = document.querySelectorAll('.select-btn');
-const addEventListener = () => {
-  selectBtns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-      console.log('clicked');
-      adjustBtn(btn);
-    });
-  });
-};
 getSearchResult();
 
-setTimeout(() => {
-  console.log(selectBtns);
-}, 2000);
-
+loginBtn.addEventListener('click', () => {
+    localStorage.removeItem('user');
+    localStorage.removeItem('flights');
+    localStorage.removeItem('departureFlights');
+    localStorage.removeItem('returnFlights');
+    window.location.href = '../pages/signin.html';
+});
