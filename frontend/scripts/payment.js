5714b0ec52b74c788ae69b436c55e0bca713acd6
George <g.m.6@live.com>
2024-03-21 04:46:58 +0200

    booking one flight fixed

--- frontend/scripts/payment.js
+++ frontend/scripts/payment.js
@@ -61,7 +61,3 @@ checkoutBtn.addEventListener('click', async () => {
 cancelBtn.addEventListener('click', () => {
     window.location.href = '../pages/booking.html';
 });
-
-continueBtn.addEventListener('click', () => {
-    window.location.href = '../index.html';
-});
